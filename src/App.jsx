bc0c27868201e662c3bd04f8a83a41f528530b31
Pradyumn-Tangirala <rocketpradyumn@gmail.com>
2026-01-09 22:58:58 +0530

    Fix deployment config for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import WaveInterference from './WaveInterference';
 import LandingPage from './LandingPage';
 import Chatbot from './Chatbot';
